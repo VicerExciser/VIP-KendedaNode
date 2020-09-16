f949fec8238519e646295e0ff4faf513eef1650e
VicerExciser <acondict11@gmail.com>
2020-09-16 05:57:53 +0000

    improvements made to usb_test from the pynq

--- vip/kendeda/air/pynq_node/tests/usb_test.py
+++ vip/kendeda/air/pynq_node/tests/usb_test.py
@@ -1,7 +1,17 @@
 import time
-from util import util, comports
-from opcn2 import opcn2
-from k33 import k33_uart as k33
+try:
+	from util import util, comports
+	from opcn2 import opcn2
+	from k33 import k33_uart as k33
+except (ImportError, ModuleNotFoundError):
+	import os, sys
+	rootpath = '/'.join(os.getcwd().split('/')[:-2])
+	print(f"[{__file__}] Appending '{rootpath}' to sys.path")
+	sys.path.append(rootpath)
+	from util import util, comports
+	from opcn2 import opcn2
+	from k33 import k33_uart as k33
+
 
 k33_usb_port = None 
 opc_usb_port = None 
@@ -17,17 +27,26 @@ for port in ports_dict.keys():
 
 ## Instantiate K33-ELG sensor:
 co2_sensor = k33.K33(port=k33_usb_port) if k33_usb_port is not None else k33.K33()
-print(f"[{__file__}] K33-ELG enabled.")
+print(f"\n[{__file__}] K33-ELG enabled.")
 
 ## Instantiate OPC-N2 sensor:
 opc_sensor = opcn2.OPC_N2(use_usb=True, usb_port=opc_usb_port) if opc_usb_port is not None else opcn2.OPC_N2()
-print(f"[{__file__}] OPC-N2 enabled.")
+print(f"\n[{__file__}] OPC-N2 enabled.")
 
 while True:
 	try:
-		co2 = co2_sensor.read_co2()
-		pm = opc_sensor.pm()
-		print(f"\nCO2 = {co2} ppm\nPM1 = {pm['PM1']} #/cc\nPM2.5 = {pm['PM2.5']} #/cc\nPM10 = {pm['PM10']} #/cc\n========================\n")
+		#co2 = co2_sensor.read_co2()
+		#pm = opc_sensor.pm()
+		#print(f"\nCO2   = {co2} ppm\nPM1   = {pm['PM1']} #/cc\nPM2.5 = {pm['PM2.5']} #/cc\nPM10  = {pm['PM10']} #/cc\n========================\n")
+
+		data = []
+		data.append(co2_sensor.read_co2())
+		data.append(opc_sensor.PM1())
+		data.append(opc_sensor.PM25())
+		data.append(opc_sensor.PM10())
+		data.append("="*30)
+		print("\nCO2   = {} ppm\nPM1   = {} #/cc\nPM2.5 = {} #/cc\nPM10  = {} #/cc\n{}\n".format(*data))
+
 		time.sleep(5)
 	except KeyboardInterrupt:
 		print(f'\n{__file__} ABORTING.\n')
