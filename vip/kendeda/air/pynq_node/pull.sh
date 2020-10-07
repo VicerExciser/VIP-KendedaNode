495dd60448f5b017d8927e9f45d4fe1dafc6f6db
VicerExciser <acondict11@gmail.com>
2020-10-07 08:55:09 +0000

    Added scripts for updating repo & OPC project files on the pynq

---
+++ vip/kendeda/air/pynq_node/pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FAILS=0
+while ! git pull; do
+	FAILS=$((FAILS+1))
+	if [ "$FAILS" -gt 4 ]; then
+		echo -e "\nERROR: Could not reach github.com -- aborting.\n"
+		exit 1
+	fi
+	sleep 3s
+done
+
--- vip/kendeda/air/pynq_node/update_opc.sh
+++ vip/kendeda/air/pynq_node/update_opc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 PYFILE="opc.py"
+LIBPATH="${HOME}/pynq/lib/arduino/"
+DIVIDER="\n\n========================================"
 FAILS=0
 while ! git pull; do
 	FAILS=$((FAILS+1))
@@ -9,15 +11,20 @@ while ! git pull; do
 	fi
 	sleep 3s
 done
-LIBPATH="/home/xilinx/pynq/lib/arduino/"
+echo -e "$DIVIDER"
 cp -v -r opc/ $LIBPATH
-cp "opc/$PYFILE $LIBPATH"
+# cp "opc/${PYFILE} ${LIBPATH}${PYFILE}"
+cp -v ${LIBPATH}opc/${PYFILE} ${LIBPATH}${PYFILE}
 CWD=$(pwd)
 cd "${LIBPATH}opc/Debug/"
 pwd
+echo -e "$DIVIDER"
 make
 cd $LIBPATH
 make
 cd $CWD
 pwd
-echo -e "\nNow run $PYFILE in $LIBPATH\n"
+# echo -e "\nNow run $PYFILE in $LIBPATH\n"
+echo -e "$DIVIDER"
+echo -e " Now run the following command:\n\n    cd $LIBPATH && sudo python3 $PYFILE"
+echo -e "$DIVIDER"
