95971431f808fafd57b9a9bd15c57ea9a9b32eff
VicerExciser <acondict11@gmail.com>
2020-10-30 22:42:23 +0000

    simpletest.py example script now polls all 4 ADC channels

--- vip/kendeda/adafruit_ads1x15/simpletest.py
+++ vip/kendeda/adafruit_ads1x15/simpletest.py
@@ -10,13 +10,17 @@ lib = MicroblazeLibrary(overlay.iop_arduino, ['i2c'])
 i2c = lib.i2c_open_device(0)
 
 ads = ADS.ADS1015(i2c)
-chan = AnalogIn(ads, ADS.P0)
+channels = [AnalogIn(ads, ADS.P0), AnalogIn(ads, ADS.P1), AnalogIn(ads, ADS.P2), AnalogIn(ads, ADS.P3)] 
 
-print("{:>5}\t{:>5}".format('raw', 'v'))
+print("[x] {:>5}\t{:>5}".format('raw', 'v'))
 try:
 	while True:
-		print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
-		time.sleep(0.5)
+		for i in range(4):
+			chan = channels[i]
+			print("[{}] {:>5}\t{:>5.3f}".format(i, chan.value, chan.voltage))
+			time.sleep(0.5)
+		print("\n")
+		time.sleep(2)
 except KeyboardInterrupt:
 	pass 
 
